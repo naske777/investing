daad3f7c3b592dd083719eb7ad705574ba7753b3
naske <naske777ig@gmail.com>
2024-06-26 00:16:56 -0400

    fix: Undo changes

--- build.sh
+++ build.sh
@@ -9,18 +9,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
-# Set the DJANGO_SETTINGS_MODULE environment variable
-export DJANGO_SETTINGS_MODULE=investing.settings
-
-# Convert static asset files
-python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
 python manage.py migrate
