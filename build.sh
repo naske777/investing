fc5693f70fc61423881d619f104533ab0c6662b7
naske <naske777ig@gmail.com>
2024-06-26 16:39:11 -0400

    fix(prod): Update build commands

--- build.sh
+++ build.sh
@@ -8,5 +8,7 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+
 # Apply any outstanding database migrations
+python manage.py makemigrations
 python manage.py migrate
