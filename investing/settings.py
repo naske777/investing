70f2e05bfebcca6741b74368bc79a19872b446aa
naske <naske777ig@gmail.com>
2024-06-26 14:44:52 -0400

    fix: Add USE_SESSION_AUTH

--- investing/settings.py
+++ investing/settings.py
@@ -15,6 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SWAGGER_DEFAULTS = {
+    
+    'USE_SESSION_AUTH': False,
+    
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
