15f672f3da8cdb9d8296d479d058cda79967b367
naske <naske777ig@gmail.com>
2024-06-26 01:07:54 -0400

    fix: structure

--- investing/settings.py
+++ investing/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_yasg',
-    'main',
+    'money',
     
 ]
 
@@ -79,12 +79,10 @@ import dj_database_url
 # Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
     'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/investing',
         conn_max_age=600
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- money/api.py
+++ money/api.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from .models import Wallet
-from .serializers import WalletSerializer
+from .serializer import WalletSerializer
 
 class WalletViewSet(viewsets.ModelViewSet):
     queryset = Wallet.objects.all()
