ea0d138155d2c1830127dea772415f2156513493
naske <naske777ig@gmail.com>
2024-06-26 21:39:25 -0400

    feat: Return wallet data for ceate transaction

--- money/api.py
+++ money/api.py
@@ -9,3 +9,19 @@ class WalletViewSet(viewsets.ModelViewSet):
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        # Obtén la instancia de la transacción para acceder a la wallet asociada
+        transaction_instance = serializer.instance
+        wallet_instance = transaction_instance.wallet
+
+        # Utiliza WalletSerializer para serializar los datos de la wallet
+        wallet_serializer = WalletSerializer(wallet_instance)
+
+        # Devuelve los datos de la wallet en lugar de los de la transacción
+        return Response(wallet_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
